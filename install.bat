f38b80a826e929f49d76224562e3bd3b558b470b
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-09-09 10:05:07 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,10 +3,12 @@ powershell.exe "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.Se
 choco install googlechrome -y
 choco install vlc -y
 choco install qbittorrent -y
-choco install libreoffice -y
-choco install sumatrapdf -y
-choco install anydesk -y
-choco install teamviewer -y
 choco install wget -y
+
+# Activator for Win10
+wget https://mega.nz/folder/3DxykIwS#qYGZKN6XVqQvpNXpF0KCFw
+# Activator for Win7
+wget https://mega.nz/folder/HS4TSKBJ#FkVslmYkHFzLZ_D4RJdRNg
+
 wget https://github.com/hellzerg/optimizer/releases/download/15.6/Optimizer-15.6.exe
 wget https://www.3dpchip.com/new/3DP_Chip_v2307.exe
