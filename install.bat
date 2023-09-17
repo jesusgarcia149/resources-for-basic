3018fb23dcee3a680c8d33742cb0c9a76e0f76c0
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-09-16 20:06:54 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -10,6 +10,8 @@ wget https://mega.nz/folder/3DxykIwS#qYGZKN6XVqQvpNXpF0KCFw
 # Activator for Win7
 wget https://mega.nz/folder/HS4TSKBJ#FkVslmYkHFzLZ_D4RJdRNg
 
+choco install driverbooster -y
+
 wget https://github.com/hellzerg/optimizer/releases/download/15.6/Optimizer-15.6.exe
 # return Calculator = Optimizer > Apps UWP > Microsoft.WindowsCalculator
 wget https://www.3dpchip.com/new/3DP_Chip_v2307.exe
